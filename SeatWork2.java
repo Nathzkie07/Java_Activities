00cce0bdbc453526901d60373324dcad190a5aec
Nathaniel F. Torrero <nftorrero@headoffice.corp.bpi.com.ph>
2025-12-04 16:49:31 +0800

    Activity 2 of Module 2

---
+++ SeatWork2.java
@@ -0,0 +1,14 @@
+package javaTraining;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhiteBoard whiteBoardObject = new WhiteBoard();
+		int result=whiteBoardObject.addNumbers(5, 3);
+		System.out.println("The total is: "+String.valueOf(result));
+		
+
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,10 @@
+package javaTraining;
+
+public class WhiteBoard {
+	
+	int addNumbers(int addend1, int addend2) {
+		int result = addend1 + addend2;
+		return result;
+	}
+
+}
